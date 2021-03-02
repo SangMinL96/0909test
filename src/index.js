f8357fce70bfa7e0d3e82684011db1da3d7b01da
SangMin96 <hwon3794@gmail.com>
2021-03-02 12:16:40 +0900

    sdfasd

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-<<<<<<< HEAD
-  //머지 테스트ㄴㅇㅁㄹㄴㅇㄻㄴㅇㄴㅁㅇㄹㄴㅇ
-=======
-  //머지 테스트ㄴㅇㅁㄹㄴㅇㄻㄴㅇㅊㅍㅌㅋㅇㅇㅇ
->>>>>>> master
+  //머지 테스트ㄴㅇㅁㄹㄴㅇㄻㄴㅇㄴㅁㅇㄹㄴㅇㄴㅁㅇㄻ
   <React.StrictMode>
     <App />
   </React.StrictMode>,
